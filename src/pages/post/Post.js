aa2526d3f586d0472aa4846c0313ef4b74f29efe
aibek23 <76870730+aibek23@users.noreply.github.com>
2021-08-17 02:03:56 +0600

    add module posts end button on form comments

--- src/pages/post/Post.js
+++ src/pages/post/Post.js
@@ -1,13 +1,49 @@
 import React from 'react';
+import Comments from '../../components/commets/Comments';
 import styles from  './Post.module.css';
 
 class Post extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            useBtn1:true,
+            useBtn2:false,
+            useBtn3:false,
+        };
+    
+        // Эта привязка обязательна для работы `this` в колбэке.
+        this.handleClick = this.handleClick.bind(this);
+      }
+    
+      handleClick(e) {
+
+          if(e.target.value==="1"){
+            this.setState({useBtn1: true})
+            this.setState({useBtn2: false})
+            this.setState({useBtn3: false})     
+          }else if(e.target.value==="2"){
+            this.setState({useBtn2: true})
+            this.setState({useBtn1: false})
+            this.setState({useBtn3: false})  
+          }else if(e.target.value==="3"){
+            this.setState({useBtn3: true})
+            this.setState({useBtn1: false})
+            this.setState({useBtn2: false}) 
+          }
+
+      }
+ 
+     
+  
+    
+ 
 
     render(){
+
         return(
             <div className="postcard-container">
                 <div className="postcard-blocks">
-                    <div className="postcard_authors">
+                     <div className="postcard_authors">
                         <span className={styles.create_name}>Тестов Тест</span>
                         <span className="create_date">31.07.2021 19:59</span>
                     </div>
@@ -33,25 +69,33 @@ class Post extends React.Component{
                         </p>
                     </div>
                 </div>
-
-                <div className="comments-blocks">
-                    <h2 className="comments-blocks__title">Комментарии <span
-                        className="comments-blocks__counter">5</span></h2>
-                    <div className="comments-blocks__form">
-                        <div className="form-control">
-                            <label htmlFor="">Имя</label>
-                            <input type="text"/>
-                        </div>
-                        <div className="form-control">
-                            <label htmlFor="">Сообщение</label>
-                            <textarea name="" id="" cols="30" rows="10"></textarea>
-                        </div>
-                        <div className="form-control">
-                            <button>Отправить</button>
-                        </div>
+                 <div className={styles.comments}>
+                    <div className={styles.title}>                         
+                        <button className={this.state.useBtn1?`${styles.active}`:""} onClick={this.handleClick} value="1">Комментарии (<span
+                        className={styles.counter}>5</span>)</button>
+                        <button className={this.state.useBtn2?`${styles.active}`:""} onClick={this.handleClick} value="2">нравится(<span
+                        className={styles.counter}>5</span>)</button>
+                        <button className={this.state.useBtn3?`${styles.active}`:""} onClick={this.handleClick} value="3">жалоба(<span
+                        className={styles.counter}>5</span>)</button>
                     </div>
-                </div>
+                    <div className={styles.form}>
+                        <p>Добавьте комментарии</p>
+                        <div className={styles.formText}>
+                            <label htmlFor=""></label>
+                            <textarea  placeholder="Сообщение" name="" id="" cols="30" rows="5" ></textarea>
+                        </div>
+                        {/* <p>ваша имя</p>
+                        <div className={styles.formText}>
+                            <input  type="text" placeholder="Имя"/>
+                        </div> */}
+                     
+                            <button className={styles.formBtn}>Отправить</button>
+                        
+                    </div>
+                </div> 
+                {this.state.useBtn1?<Comments/>:""} 
             </div>
+
         )
     }
 }
